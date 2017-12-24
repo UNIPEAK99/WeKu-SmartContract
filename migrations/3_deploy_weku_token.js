459b0deaccb7b755d6ab5f17d600b2076bd0d4a4
AlexeyQian <alexeyqian@gmail.com>
2017-12-24 23:44:12 +0800

    fix coin decimal issue

--- migrations/3_deploy_weku_token.js
+++ migrations/3_deploy_weku_token.js
@@ -1,7 +1,7 @@
 var WEKUToken = artifacts.require("./WEKUToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(WEKUToken, "0x627306090abaB3A6e1400e9345bC60c78a8BEf57"); 
+  deployer.deploy(WEKUToken); 
 };
 
 
