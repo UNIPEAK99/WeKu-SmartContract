cec77c3523d3e0e67a58d9966b4aab6dd8591767
AlexeyQian <alexeyqian@gmail.com>
2018-01-14 23:39:56 +0800

    add crowdsale smart contract

--- truffle.js
+++ truffle.js
@@ -11,6 +11,13 @@ module.exports = {
       host: "demo.weku.io",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: "demo.weku.io", // Connect to geth on the specified
+      port: 8545,
+      from: "0x461d98d4dc9109a919c56c8f48d4522de9a73a61", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
     }
   }
 };
